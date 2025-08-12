7cb8298ac99e0883040c6b0e8f15a04c4c22708f
stelvyanselmo <stelvyanselmo@gmail.com>
2025-08-12 23:20:48 +0100

    deploy: api service

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     .addTag('DP - Banks')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('renda-extra-api', app, documentFactory);
+  SwaggerModule.setup('renda', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 
